c6dbec7d0b4dd1e8a35e309c227e7e0ef12c6c60
watch14 <watchman1414@gmail.com>
2024-03-18 07:56:59 +0100

    home_page choose ingrediant done

--- FeedME/src/app/app.component.ts
+++ FeedME/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { RandomRecipesComponent } from './random-recipes/random-recipes.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, HomePageComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, HomePageComponent, RandomRecipesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ FeedME/src/app/random-recipes/random-recipes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-random-recipes',
+  standalone: true,
+  imports: [],
+  templateUrl: './random-recipes.component.html',
+  styleUrl: './random-recipes.component.css'
+})
+export class RandomRecipesComponent {
+
+}
