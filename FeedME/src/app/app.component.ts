03beaf07e53229a27d0b786268182bafb57557ed
watch14 <watchman1414@gmail.com>
2024-03-20 01:02:37 +0100

    routes in route

--- FeedME/src/app/app.component.ts
+++ FeedME/src/app/app.component.ts
@@ -6,11 +6,20 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { RandomRecipesComponent } from './random-recipes/random-recipes.component';
 import { RecipeComponent } from './recipe/recipe.component';
 import { CountryComponent } from './country/country.component';
+import { FavoritesComponent } from './favorites/favorites.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, HomePageComponent, RandomRecipesComponent, RecipeComponent, CountryComponent],
+  imports: [RouterOutlet, 
+            HeaderComponent, 
+            FooterComponent, 
+            HomePageComponent, 
+            RandomRecipesComponent, 
+            RecipeComponent, 
+            CountryComponent,
+            FavoritesComponent
+          ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- FeedME/src/app/app.routes.ts
+++ FeedME/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { RandomRecipesComponent } from './random-recipes/random-recipes.component';
+import { RecipeComponent } from './recipe/recipe.component';
+import { FavoritesComponent } from './favorites/favorites.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {'path': '', component:HomePageComponent},
+    {'path': 'random_recipes', component:RandomRecipesComponent},
+    {'path': 'recipe' , component:RecipeComponent},
+    {'path': 'favorites', component:FavoritesComponent},
+];
