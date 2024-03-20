fc75a53ac505d0c4246b6acddbbcda685daa7214
Iheb Chaoued <ihebchaoued@gmail.com>
2024-03-20 03:18:26 +0100

    Updated

--- FeedME/src/app/favorites/favorites.component.ts
+++ FeedME/src/app/favorites/favorites.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-favorites',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './favorites.component.html',
   styleUrl: './favorites.component.css'
 })
