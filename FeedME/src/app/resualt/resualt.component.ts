4c3e6d7af4242f35d0e1236f446ae8d456d0bf72
Iheb Chaoued <ihebchaoued@gmail.com>
2024-03-20 03:50:03 +0100

    routing Updated

--- FeedME/src/app/resualt/resualt.component.ts
+++ FeedME/src/app/resualt/resualt.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-resualt',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './resualt.component.html',
   styleUrl: './resualt.component.css'
 })
